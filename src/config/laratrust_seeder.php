bc319c6310a455168e878d5cf3c711beb402f996
Caleb Flores <kleb.13@gmail.com>
2020-06-17 13:50:32 -0600

    add roles in laratrust seeder config

--- src/config/laratrust_seeder.php
+++ src/config/laratrust_seeder.php
@@ -12,20 +12,9 @@ return [
     'truncate_tables' => true,
 
     'roles_structure' => [
-        'superadministrator' => [
-            'users' => 'c,r,u,d',
-            'payments' => 'c,r,u,d',
-            'profile' => 'r,u'
-        ],
-        'administrator' => [
-            'users' => 'c,r,u,d',
-            'profile' => 'r,u'
+        'admin' => [
         ],
         'user' => [
-            'profile' => 'r,u',
-        ],
-        'role_name' => [
-            'module_1_name' => 'c,r,u,d',
         ]
     ],
 
