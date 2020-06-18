cf90fbb0cb6cb35af8fbcac3c78505c5595cabd3
Caleb Flores <kleb.13@gmail.com>
2020-06-18 17:07:47 -0600

    added movie model

---
+++ src/app/Models/Movie.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Movie
+ * @package App\Models
+ * @property integer id;
+ * @property integer stock;
+ * @property string title;
+ * @property string description;
+ * @property boolean availability;
+ * @property float sales_price;
+ * @property float rental_price;
+ */
+class Movie extends Model
+{
+
+
+}
---
+++ src/database/migrations/2020_06_18_224836_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->integer("stock");
+            $table->boolean("availability");
+            $table->string("title");
+            $table->string("description");
+            $table->float("rental_price");
+            $table->float("sale_price");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
