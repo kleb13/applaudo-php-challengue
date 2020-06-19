10f88b1f34678614592f96a4743f972a0f759700
Caleb Flores <kleb.13@gmail.com>
2020-06-18 19:28:56 -0600

    add movie factory

---
+++ src/database/factories/MovieFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Movie;
+use Faker\Generator as Faker;
+
+$factory->define(Movie::class, function (Faker $faker) {
+    return [
+    "stock" => $faker->numberBetween(1,50),
+    "title" => $faker->title,
+    "description" => $faker->paragraph,
+    "availability" => $faker->boolean,
+    "sale_price" => $faker->randomFloat(2,9,80),
+    "rental_price" => $faker->randomFloat(2,5,15)
+    ];
+});
