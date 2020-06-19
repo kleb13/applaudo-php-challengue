59d9e9c7bd01b833153c9b251d6048fbe86f4dcf
Caleb Flores <kleb.13@gmail.com>
2020-06-19 07:54:18 -0600

    add movie resource

---
+++ src/app/Http/Resources/Movie.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class Movie
+ * @package App\Http\Resources
+ *
+ * @property integer id
+ * @property integer stock
+ * @property string title
+ * @property string description
+ * @property boolean availability
+ * @property float sale_price
+ * @property float rental_price
+ */
+class Movie extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        /**
+         * @var App\Models\User  $user
+         */
+        $user = auth()->user();
+        $showAvailability = empty($user)?false: $user->hasRole('admin');
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'rental_price' => $this->rental_price,
+            'sale_price' => $this->sale_price,
+            'availability' => $this->when($showAvailability,$this->availability),
+            'images' => MovieImage::collection($this->whenLoaded("images"))
+        ];
+    }
+}
