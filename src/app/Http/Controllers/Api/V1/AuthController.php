9f2a9aaea2113f084fcbaca37443feeffbfac427
Caleb Flores <kleb.13@gmail.com>
2020-06-19 20:26:25 -0600

    using jwt auth instead of simple auth

--- src/app/Http/Controllers/Api/V1/AuthController.php
+++ src/app/Http/Controllers/Api/V1/AuthController.php
@@ -4,23 +4,22 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
-use Illuminate\Contracts\Auth\Factory as AuthFactory;
+use Tymon\JWTAuth\JWTAuth as Auth;
 use Illuminate\Http\JsonResponse;
-use Psy\Util\Json;
 
 class AuthController extends Controller
 {
 
     /**
-     * @var \Illuminate\Contracts\Auth\Factory
+     * @var Auth
      */
     protected $auth;
 
     /**
      * AuthController constructor.
-     * @param AuthFactory $auth
+     * @param Auth $auth
      */
-    public function __construct(AuthFactory $auth)
+    public function __construct(Auth $auth)
     {
         $this->auth = $auth;
     }
