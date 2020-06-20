32b084f0c4a3ed9ad6c3964c3009ce98c26e4e3a
Caleb Flores <kleb.13@gmail.com>
2020-06-20 15:48:50 -0600

    add buy functionality

--- src/app/Http/Controllers/Api/V1/MovieController.php
+++ src/app/Http/Controllers/Api/V1/MovieController.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Contracts\MovieStore;
 use App\Http\Controllers\Controller;
 use App\Models\Movie;
 use App\Http\Resources\MovieCollection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     protected $movies;
-
-    public function __construct(Movie $movies)
+    protected $movieStore;
+    public function __construct(Movie $movies,MovieStore $movieStore)
     {
         $this->movies = $movies;
+        $this->movieStore = $movieStore;
     }
 
     /**
@@ -32,4 +35,12 @@ class MovieController extends Controller
         return new \App\Http\Resources\Movie($this->movies->find($id));
     }
 
+    public function buy($id)
+    {
+        $movie = $this->movies->find($id);
+        $result  = $this->movieStore->buy($movie);
+
+        return (new JsonResponse($result))
+            ->setStatusCode($result->wasSuccess()?200:400);
+    }
 }
