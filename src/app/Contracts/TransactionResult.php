d7c5f83bbcba6bc9197b298fcb459be5c57d6823
Caleb Flores <kleb.13@gmail.com>
2020-06-19 23:41:22 -0600

    add transaction result

---
+++ src/app/Contracts/TransactionResult.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Contracts;
+
+
+class TransactionResult
+{
+    /**
+     * @var boolean
+     */
+    protected $success;
+    /**
+     * @var string $message
+     */
+    protected $message;
+
+    /**
+     * TransactionResult constructor.
+     * @param bool $sucess
+     * @param string $message
+     */
+    public function __construct(bool $success, string $message)
+    {
+        $this->success = $success;
+        $this->message = $message;
+    }
+
+    /**
+     * @return bool
+     */
+    public function wasSuccess():bool
+    {
+        return $this->success;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage() :string
+    {
+        return $this->message;
+    }
+}
