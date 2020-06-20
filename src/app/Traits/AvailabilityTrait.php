e87ff324daf6abd117b0b873da0d1d37fef849a0
Caleb Flores <kleb.13@gmail.com>
2020-06-19 21:40:43 -0600

    add filter functionality to traits

--- src/app/Traits/AvailabilityTrait.php
+++ src/app/Traits/AvailabilityTrait.php
@@ -5,6 +5,7 @@ namespace App\Traits;
 
 
 use App\Scopes\AvailabilityScope;
+use Illuminate\Database\Eloquent\Builder;
 
 trait AvailabilityTrait
 {
@@ -12,5 +13,25 @@ trait AvailabilityTrait
     public static function bootAvailabilityTrait()
     {
         static::addGlobalScope(new AvailabilityScope());
+    }
+
+    public function scopeUnavailable(Builder $query)
+    {
+        return $query->withoutGlobalScope(AvailabilityScope::class)
+            ->where("availability","=",0);
+    }
+
+    public function scopeAvailable(Builder $query)
+    {
+        return $query->withoutGlobalScope(AvailabilityScope::class)
+            ->where("availability","=",1);
+    }
+
+    public function scopeFilter(Builder $query,$filter)
+    {
+        if(!in_array($filter,['available','unavailable'])){
+            return $query->withoutGlobalScope(AvailabilityScope::class);
+        }
+        return $this->{'scope'.ucfirst($filter)}($query);
     }
 }
