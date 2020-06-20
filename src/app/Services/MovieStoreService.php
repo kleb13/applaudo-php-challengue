28807c20c1f1dd31d12fb60bdef87d61868da2e6
Caleb Flores <kleb.13@gmail.com>
2020-06-20 15:53:18 -0600

    add buy method to movie store service

--- src/app/Services/MovieStoreService.php
+++ src/app/Services/MovieStoreService.php
@@ -4,20 +4,19 @@
 namespace App\Services;
 
 
-use App\Contracts\MovieStore;
-use App\Contracts\NesTransactionResult;
-use App\Contracts\OkTransanctionResult;
-use App\Contracts\TransactionResult;
-use App\Models\Movie;
+use Illuminate\Contracts\Auth\Factory as Auth;
+use App\Contracts\{MovieStore,NesTransactionResult,OkTransanctionResult,TransactionResult};
+use App\Models\{Movie,MovieTransaction};
 use Illuminate\Database\DatabaseManager as DB;
 
 class MovieStoreService implements MovieStore
 {
     protected $db;
-
-    public function __construct(DB $db)
+    protected $auth;
+    public function __construct(DB $db, Auth $auth)
     {
         $this->db = $db;
+        $this->auth = $auth;
     }
 
     /**
@@ -31,7 +30,26 @@ class MovieStoreService implements MovieStore
             return new NesTransactionResult();
         }
 
-        return new OkTransanctionResult();
+        try {
+            $this->db->beginTransaction();
+            $movie->stock -= 1;
+            if($movie->stock === 0 && $movie->availability) {
+                $movie->availability = false;
+            }
+            $movie->save();
+
+            $movie->transactions()->save(new MovieTransaction([
+                "reason" => MovieTransaction::BUY,
+                "amount" => $movie->sale_price,
+                "user_id" => $this->auth->guard()->user()->getAuthIdentifier()
+            ]));
+
+            $this->db->commit();
+            return new OkTransanctionResult();
+        }catch (\Exception $exception){
+            $this->db->rollBack();
+            return new TransactionResult(false,"something went wrong");
+        }
     }
 
     public function rent(Movie $movie): TransactionResult
