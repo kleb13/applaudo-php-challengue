2ac252de6ea1c70dae4f497a0e97e49f76854d17
Caleb Flores <caleb.flores@tegraglobal.com>
2020-06-21 14:58:39 -0600

    add basic configuration for the app

---
+++ src/setup-app.sh
@@ -0,0 +1,8 @@
+php artisan key:generate
+php artisan jwt:secret
+php artisan migrate
+php artisan db:seed
+php artisan db:seed --class=UserTableSeeder
+php artisan db:seed --class=MovieSeeder
+npm run development
+php artisan app:create-admin
